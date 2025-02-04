75118cb6ef35d502f9f1abe537d568932b36acfe
MagicJJ34 <milosz.makiel3@gmail.com>
2025-02-05 00:28:17 +0100

    Nazwanie zadań wykonywanych po naciśnięciu

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,35 @@ namespace Projekt
         {
             InitializeComponent();
         }
+
+        private void stwórz_plik_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dodaj_zadanie_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void usuñ_zadanie_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void edytuj_zadanie_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zapisz_plik_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wyjœcie_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
